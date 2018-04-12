1b4179a75e4ebe425037c239d67412514255c671
Verchasve <abhiswee@gmail.com>
2018-04-12 15:24:31 -0400

    Google Map embedded

--- BigDataDominance/src/graphics/MainGUI.java
+++ BigDataDominance/src/graphics/MainGUI.java
@@ -12,7 +12,8 @@ import java.awt.*;
 public class MainGUI extends JFrame implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
-	public  static JComboBox<String> selectedDevice ;
+	public  static JComboBox<String> selectedCuisine ;
+	public  static JComboBox<String> selectedScore ;
 	private final String American = "American";
 	private final String Italian  = "Italian";
 	private final String Mexican  = "Mexican";
@@ -28,74 +29,92 @@ public class MainGUI extends JFrame implements ActionListener{
 	private   void placeComponents1(JPanel panel) {
 		// TODO Auto-generated method stub
 		
-
+		int x = 10 ;
+		int y = 40;
+		
+ 
 		JLabel locationLabel = new JLabel("Location");
-		locationLabel.setBounds(10, 10, 100, 25);
+		locationLabel.setBounds(x, y, 100, 25);
 		panel.add(locationLabel);
 
 		JTextField city = new JTextField(10);
-		city.setBounds(100, 10, 25, 25);
+		city.setBounds(x+50, y, 25, 25);
 		panel.add(city);
 	 
 
 		JButton SearchButton = new JButton("Search");
-		SearchButton.setBounds(220, 10, 90, 25);
+		SearchButton.setBounds(220, y, 90, 25);
 		panel.add(SearchButton);
 		
 		JLabel GeoLabel = new JLabel("Cuisine");
-		GeoLabel.setBounds(10, 40, 100, 25);
+		GeoLabel.setBounds(10, y, 100, 25);
 		panel.add(GeoLabel);
  
 		
-		selectedDevice = new JComboBox<String>();
+		selectedCuisine = new JComboBox<String>();
 		
-		selectedDevice.setBounds(100, 40, 100, 25);	
-		selectedDevice.addItem("Select");
-		selectedDevice.addItem(American);
-		selectedDevice.addItem(Italian);
-		selectedDevice.addItem(Mexican);
-		selectedDevice.addItem(FastFood);
+		selectedCuisine.setBounds(100, 40, 100, 25);	
+		selectedCuisine.addItem("Select");
+		selectedCuisine.addItem(American);
+		selectedCuisine.addItem(Italian);
+		selectedCuisine.addItem(Mexican);
+		selectedCuisine.addItem(FastFood);
  
-	    selectedDevice.addActionListener(this);
-		panel.add(selectedDevice);
+		selectedCuisine.addActionListener(this);
+		panel.add(selectedCuisine);
 		
 		
-		JLabel Range = new JLabel("Distance");
-		Range.setBounds(10, 65, 60, 25);
+		JLabel Range = new JLabel("Distance(mile)");
+		Range.setBounds(10, y, 60, 25);
 		panel.add(Range);
 		
 		JTextField RangeValue = new JTextField(5);
-		RangeValue.setBounds(100, 65, 20, 25);
+		RangeValue.setBounds(100, y, 20, 25);
 		panel.add(RangeValue);
 		
 		
-		JLabel Price = new JLabel("Price");
-		Price.setBounds(120, 65, 100, 25);
+		JLabel Price = new JLabel("Price($)");
+		Price.setBounds(120, y, 100, 25);
 		panel.add(Price);
 		
 		JTextField PriceValue = new JTextField(5);
-		PriceValue.setBounds(100, 65, 20, 25);
+		PriceValue.setBounds(100, y, 20, 25);
 		panel.add(PriceValue);
 		
-		JButton SaveButton = new JButton("Update");
-		SaveButton.setBounds(220, 40, 80, 25);
-		panel.add(SaveButton);
-	 
+		JLabel Score = new JLabel("Score");
+		Score.setBounds(120, y, 100, 25);
+		panel.add(Score);
 		
-
+		selectedScore = new JComboBox<String>();
+		
+		selectedScore.setBounds(140, y, 100, 25);	
+		selectedScore.addItem("Select");
+		selectedScore.addItem("5");
+		selectedScore.addItem("4");
+		selectedScore.addItem("3");
+		selectedScore.addItem("2");
+		selectedScore.addItem("1");
+ 
+		selectedScore.addActionListener(this);
+		panel.add(selectedScore);
+		
+		JButton SaveButton = new JButton("Update");
+		SaveButton.setBounds(220, y, 80, 25);
+		panel.add(SaveButton);
+ 
  
 	}
 	
 	public   void setList()
 	{	
-		selectedDevice.addItemListener(new ItemListener(){
+		selectedCuisine.addItemListener(new ItemListener(){
 
 			@Override
 			public void itemStateChanged(ItemEvent e) 
 			{
 				// TODO Auto-generated method stub
 				
-			Object item = selectedDevice.getSelectedItem();
+			Object item = selectedCuisine.getSelectedItem();
 			
 			if(!e.equals("Select"))
 			{
@@ -103,9 +122,9 @@ public class MainGUI extends JFrame implements ActionListener{
 			{
 				
  
-				selectedDevice.removeAllItems();
+				selectedCuisine.removeAllItems();
 				
-				selectedDevice.setVisible(false);
+				selectedCuisine.setVisible(false);
 			
 			}
 			 
@@ -142,15 +161,18 @@ public class MainGUI extends JFrame implements ActionListener{
 		
 		
 		JPanel panel = new JPanel();
+		
+		
+		
 		panel.setLayout(new FlowLayout());
 	  
-		placeComponents1(panel);
+		
 		
 		
         frame.add(panel, BorderLayout.NORTH);
 	    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.setSize(1300, 900);
-		
+		placeComponents1(panel);
 		
 		   JPanel panel2 = new JPanel();
 		   panel2.setBounds(10,180,250,250);  
@@ -161,12 +183,12 @@ public class MainGUI extends JFrame implements ActionListener{
 	       
 	       frame.add(mapView);
  
-        frame.setLocationRelativeTo(null);
+        //frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 		frame.setResizable(true);
-		frame.repaint();
+		/*frame.repaint();
 		 
-		frame.validate();
+		frame.validate();*/
 	
 
 		
